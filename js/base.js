2c55062a0c1f4f7fa590d386ced2af62f3ebfb67
Allen He <allenhe97@yahoo.com>
2016-08-16 15:38:04 -0400

    Completed basic structure, no styling yet

---
+++ js/base.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+	var activeContents = 'nameCircle'
+
+	$('#navbar li').click(function(){
+		var newContents = $(this).attr('id')
+		changeContents(newContents, activeContents);
+		activeContents = newContents;
+	});
+
+	$('#nameCircle').click(function(){
+		$('#navbar ul').slideToggle(500);
+	});
+});
+
+function changeContents(newContents, activeContents){
+	$('#contents-' + activeContents).fadeToggle(300, function(){
+		$('#contents-' + newContents).fadeToggle(300);
+	});
+	$('#'+activeContents).removeClass('active');
+	$('#'+newContents).addClass('active');
+}
