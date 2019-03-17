ded7e14e0ff8b91312b6a508e1f8e41e7c9cf30c
Allen He <allenhe97@yahoo.com>
2019-03-16 23:35:41 -0400

    Quotes

--- js/base.js
+++ js/base.js
@@ -1,5 +1,5 @@
 $(function(){
 	$("#header-text, #down-arrow").click(function(){
-		$("body").animate({"scrollTop": $('#intro').offset().top}, 800);
+		$("body").animate({"scrollTop": $("#intro").offset().top}, 800);
 	});
 })
