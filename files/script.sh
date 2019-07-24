b29999f2c4e4f476c520704cb6dd416f8f6f6e27
Allen He <allenhe97@yahoo.com>
2019-07-24 13:27:35 -0400

    Update script.sh

--- files/script.sh
+++ files/script.sh
@@ -1,21 +1,22 @@
 #!/bin/bash
-createTunnel() {
-  /usr/bin/ssh -i "~/bongo.pem" -o ConnectTimeout=60 -N -R 2222:localhost:22 ubuntu@18.222.75.162
-  if [[ $? -eq 0 ]]; then
-    echo Tunnel to jumpbox created successfully
-  else
-    echo An error occurred creating a tunnel to jumpbox. RC was $?
-  fi
-}
-/bin/pidof ssh
-# Comment out createTunnel when ssh into RPI not needed
-if [[ $? -ne 0 ]]; then
-  echo Creating new tunnel connection
-  createTunnel
-fi
+# createTunnel() {
+#   /usr/bin/ssh -i "~/bongo.pem" -o ConnectTimeout=60 -N -R 2222:localhost:22 ubuntu@18.222.75.162
+#   if [[ $? -eq 0 ]]; then
+#     echo Tunnel to jumpbox created successfully
+#   else
+#     echo An error occurred creating a tunnel to jumpbox. RC was $?
+#   fi
+# }
+# /bin/pidof ssh
+# # Comment out createTunnel when ssh into RPI not needed
+# if [[ $? -ne 0 ]]; then
+#   echo Creating new tunnel connection
+#   createTunnel
+# fi
 
-cd /home/ubuntu &&
-. /home/ubuntu/hardware.config
-date > smth.txt
-scp -o StrictHostKeyChecking=no -i "${ec2keyfile}" smth.txt "${ec2hostname}:~/"
-scp -o StrictHostKeyChecking=no -i "${ec2keyfile}" every_5_min.log "${ec2hostname}:~/"
+# cd /home/ubuntu &&
+# . /home/ubuntu/hardware.config
+# date > smth.txt
+# scp -o StrictHostKeyChecking=no -i "${ec2keyfile}" smth.txt "${ec2hostname}:~/"
+# scp -o StrictHostKeyChecking=no -i "${ec2keyfile}" every_5_min.log "${ec2hostname}:~/"
+reboot
