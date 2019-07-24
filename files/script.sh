76b09f2100185cd028e131627177cf57469d317b
Allen He <allenhe97@yahoo.com>
2019-07-24 14:36:37 -0400

    Update script.sh

--- files/script.sh
+++ files/script.sh
@@ -4,6 +4,7 @@ cd /home/ubuntu &&
 . /home/ubuntu/hardware.config
 date > smth.txt
 scp -o StrictHostKeyChecking=no -i "${ec2keyfile}" smth.txt "${ec2hostname}:~/"
+scp -o StrictHostKeyChecking=no -i "${ec2keyfile}" every_5_min.log "${ec2hostname}:~/"
 
 createTunnel() {
   /usr/bin/ssh -i "~/bongo.pem" -o ConnectTimeout=60 -N -R 2222:localhost:22 ubuntu@18.222.75.162
@@ -16,12 +17,10 @@ createTunnel() {
 
 /bin/pidof ssh
 
-# # Comment out when do not want to createTunnel 
-# if [[ $? -ne 0 ]]; then
-#   echo Creating new tunnel connection
-#   createTunnel
-# fi
+# Comment out when do not want to createTunnel 
+if [[ $? -ne 0 ]]; then
+  echo Creating new tunnel connection
+  createTunnel
+fi
 
-scp -o StrictHostKeyChecking=no -i "${ec2keyfile}" every_5_min.log "${ec2hostname}:~/"
-
-sudo reboot
+# sudo reboot
